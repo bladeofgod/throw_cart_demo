87b7786e93bcfb28c81bb7a3685cac1c846ab0c3
lijiaqi <lijiaqi@188yd.com>
2019-12-03 18:00:05 +0800

    1.0

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mall_cart/parabola_animate_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  GlobalKey floatKey = GlobalKey();
+  GlobalKey rootKey = GlobalKey();
+  Offset floatOffset ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      RenderBox renderBox = floatKey.currentContext.findRenderObject();
+      floatOffset = renderBox.localToGlobal(Offset.zero);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Container(
+        key: rootKey,
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey,
+        child: ListView(
+          children: List.generate(40, (index){
+            return generateItem(index);
+          }).toList(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow,
+        key: floatKey,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+  Widget generateItem(int index){
+
+    //GlobalKey itemKey = GlobalKey();
+    Text text = Text("item $index",style: TextStyle(fontSize:
+    25),);
+
+
+//    RaisedButton rb  = RaisedButton(
+//      key: itemKey,
+//      color: Colors.blue,
+//      child: Text("button $index"),
+//      onPressed: (){
+//        setState(() {
+//          OverlayEntry entry = OverlayEntry(
+//            builder: (ctx){
+//              return ParabolaAnimateWidget(rootKey,itemKey,floatKey);
+//            }
+//          );
+//          Overlay.of(rootKey.currentContext).insert(entry);
+//        });
+//
+//      },
+//    );
+    Offset temp;
+    return GestureDetector(
+      onPanDown: (details){
+        temp = new Offset(details.globalPosition.dx, details.globalPosition
+            .dy);
+      },
+      onTap: (){
+        Function callback ;
+        setState(() {
+          OverlayEntry entry = OverlayEntry(
+              builder: (ctx){
+//                RenderBox renderBox = itemKey.currentContext.findRenderObject();
+//                Offset itemOffset = renderBox.localToGlobal(Offset.zero,
+//                    ancestor: rootKey.currentContext.findRenderObject());
+//                print("item offset  :${itemOffset.toString()}    float offset"
+//                    " ${floatOffset.toString()}");
+                // root key：根widget key， 主要用于定位
+                // temp:点击坐标，开始位置。floatOffset 结束坐标
+                //Icon：传入想弹出的widget
+                //call back: 会回传一个动画执行状态
+                //duration： 动画时间 可选，默认1秒
+                return ParabolaAnimateWidget(rootKey,temp,floatOffset,
+                    Icon(Icons.cancel,color: Colors.greenAccent,),callback,);
+              }
+          );
+
+          callback = (status){
+            if(status == AnimationStatus.completed){
+              entry?.remove();
+            }
+          };
+
+          Overlay.of(rootKey.currentContext).insert(entry);
+        });
+      },
+      child: Container(
+        color: Colors.orange,
+        child: text,
+      ),
+    );
+
+
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/parabola_animate_widget.dart
@@ -0,0 +1,153 @@
+
+
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ParabolaAnimateWidget extends StatefulWidget{
+  final GlobalKey rootKey;
+  final Offset startOffset;
+  final Offset endOffset;
+  final Widget animateWidget;
+  final Function animateCallback;
+  final int duration;
+
+
+  ParabolaAnimateWidget(this.rootKey,this.startOffset,this.endOffset,this
+      .animateWidget,this.animateCallback,{int duration = -1})
+    : this.duration = duration,assert(animateWidget != null);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ParabolaAnimateWidgetState(rootKey,startOffset,endOffset,
+      animateWidget,animateCallback,this.duration);
+  }
+
+}
+
+class ParabolaAnimateWidgetState extends State<ParabolaAnimateWidget> with
+    SingleTickerProviderStateMixin {
+  final GlobalKey rootKey;
+  final Offset startOffset;
+  final Offset endOffset;
+  final Widget animateWidget;
+  final Function animateCallback;
+  int _animationDuration ;
+
+  ParabolaAnimateWidgetState(this.rootKey,this.startOffset,this.endOffset,
+  this.animateWidget,this.animateCallback,int duration)
+    : _animationDuration = duration == -1 ? 1 : duration;
+
+
+
+  AnimationController _controller;
+  Animation _animation;
+
+
+  double widgetLeft = 0.0;
+  double widgetTop = 0.0;
+
+  double animateLen = 0.0;
+  PathMetric animatePM ;
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(vsync: this,duration: Duration(seconds:
+    _animationDuration));
+
+
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      generateAnimateAttr();
+      startAnimation();
+    });
+
+  }
+
+  generateAnimateAttr(){
+    //RenderBox startRB = startKey.currentContext.findRenderObject();
+//    Offset startOffset = startRB.localToGlobal(Offset.zero,ancestor: rootKey.currentContext.findRenderObject());
+//    RenderBox endRB = endKey.currentContext.findRenderObject();
+//    Offset endOffset = endRB.localToGlobal(Offset.zero,ancestor: rootKey.currentContext.findRenderObject());
+    Path path = getPath(startOffset, endOffset);
+    PathMetrics pms = path.computeMetrics();
+    animatePM = pms.elementAt(0);
+    animateLen = animatePM.length;
+
+  }
+
+  Path getPath(Offset start,Offset end) {
+
+    //print("start offset :${start.toString()}");
+    Path path = Path();
+    path.moveTo(start.dx,start.dy);
+    path.quadraticBezierTo(
+        end.dx / 2, start.dy, end.dx, end.dy);
+    //path.cubicTo(size.width / 4, 3 * size.height / 4, 3 * size.width / 4, size.height / 4, size.width, size.height);
+
+    return path;
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Positioned(
+      left: widgetLeft,
+      top: widgetTop,
+      child: animateWidget,
+    );
+  }
+
+
+  startAnimation(){
+
+    _animation = Tween(begin: 0.0,end: animateLen).animate(_controller)
+        ..addListener((){
+          //print("animation value ${_animation.value}");
+          double tempStart = _animation.value;
+          Tangent t = animatePM.getTangentForOffset(tempStart);
+          setState(() {
+            widgetLeft = t.position.dx;
+            widgetTop = t.position.dy;
+          });
+        })
+        ..addStatusListener((status){
+          animateCallback(status);
+          if(status == AnimationStatus.completed){
+            _controller?.stop();
+            _controller?.dispose();
+          }
+        });
+    _controller.forward();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
